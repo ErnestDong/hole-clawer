89fcafba7371c8708c31e6249fd899352e70f7da
Ernest DONG <dongcy2000@outlook.com>
2022-02-16 16:34:14 +0800

    sqlite init

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### 获得你的 token 
 写入 [app.py](./app.py) 中相应位置
 ### 安装依赖
+数据持久化采用 [sqlite](https://www.sqlite.org/index.html)，请自行安装
 ```shell
 # 我使用
 poetry install
@@ -18,6 +19,5 @@ python app.py
 
 ## 未来计划
 
-- TODO 数据持久化到数据库或 excel 中
 - TODO 改为多进程加快速度
 - TODO 人机验证（目前只遇到一次，大意了没有闪验证通过了）
--- hole/parse_data.py
+++ hole/parse_data.py
@@ -2,6 +2,10 @@ import logging
 from hole.fillin_list import GetList
 from hole.get_comment import GetComment
 import time
+from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine("sqlite:///hole.db")
 
 
 logging.config.fileConfig("log/logging.conf")
@@ -17,13 +21,25 @@ class App:
     def run(self):
         hole_list = GetList(self.user_token)
         pval = 1
+        hole_list_data = []
         for pval in range(1, 10):
-            hole_list_data = hole_list.get_data(p_key="p", p_val=pval)
+            hole_list_data += hole_list.get_data(p_key="p", p_val=pval)
+            logger.info("Get hole list data")
             logger.info(hole_list_data)
-            comment_list = GetComment(self.user_token, hole_list_data)
-            comment_data = comment_list.get_data()
-            logger.info(comment_data)
+        hole_list_df = pd.DataFrame(hole_list_data)
+        hole_list_df.to_sql("list", engine, if_exists="append")
+        comment_list = GetComment(self.user_token, hole_list_data)
+        comment_data = comment_list.get_data()
+        comment_df = pd.DataFrame(comment_data)
+        comment_df.to_sql("comment", engine, if_exists="append")
+        logger.info(comment_data)
         time.sleep(self.sleep_time)
+        hole_list = pd.read_sql("select * from list", engine)
+        hole_list.drop_duplicates(subset=["pid"], inplace=True, keep="last")
+        hole_list.to_sql("list", engine, if_exists="replace")
+        comment = pd.read_sql("select * from comment", engine)
+        comment.drop_duplicates(subset=["cid"], inplace=True, keep="last")
+        comment.to_sql("comment", engine, if_exists="replace")
 
     def run_thread(self):
         logger.info("Start")
